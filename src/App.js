531c0d8c221e3ff233b0485084f26bc00f567cab
AbdoArafh <abdoarafh54@gmail.com>
2021-08-01 17:31:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Doughnut } from 'react-chartjs-2';
+
+const data = {
+  labels: ['data1', 'data2', 'data3', 'data4', 'data5', 'data6'],
+  datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+          'rgb(0, 0, 255)',
+          'rgb(0, 40, 240)',
+          'rgb(0, 80, 225)',
+          'rgb(0, 120, 210)',
+          'rgb(0, 160, 200)',
+          'rgb(0, 200, 190)'
+      ],
+      borderWidth: 1
+  }]
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: "300px"}}>
+      <Doughnut data={data} width={100} options={{cutout: "90%", responsive: true, maintainAspectRatio: true}}/>
     </div>
   );
 }
